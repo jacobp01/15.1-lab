02c6f4d0c8c87de48f74d13de0e35da8613c1073
jacobp01 <34072284+jacobp01@users.noreply.github.com>
2017-12-11 11:05:12 -0800

    day2 work
    implemented area / perimeter methods

--- IrregularPolygon.java
+++ IrregularPolygon.java
@@ -1,6 +1,6 @@
 import java.awt.geom.*;     // for Point2D.Double
 import java.util.ArrayList; // for ArrayList
-// import gpdraw.*;
+import gpdraw.*;
 
 public class IrregularPolygon{
    private ArrayList <Point2D.Double> myPolygon;
@@ -15,12 +15,38 @@ public class IrregularPolygon{
    public void draw() { }
 
    public double perimeter() {
-       int perimeter = 0;
-       for(Point2D.Double pt: myPolygon){
-           pt.getX = "x1";
-           pt.getY = "y1";
+       double perimeter = 0.0;
+       for(int i = 0; i < myPolygon.size() - 1; i++){
+           double x1 = myPolygon.get(i).getX();
+           double x2 = myPolygon.get(i+1).getX();
+           double y1 = myPolygon.get(i).getY();
+           double y2 = myPolygon.get(i+1).getY();
+           perimeter += Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
         }
+       for(int i = myPolygon.size() - 1; i < myPolygon.size(); i++){
+           double x1 = myPolygon.get(i).getX();
+           double x2 = myPolygon.get(0).getX();
+           double y1 = myPolygon.get(i).getY();
+           double y2 = myPolygon.get(0).getY();
+           perimeter += Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+        }
+       return perimeter; 
    }
 
-   public double area() { }
+   public double area() {
+    double area = 0.0;
+    double total = 0.0;
+        for(int i = 0; i < myPolygon.size() - 1; i++){
+           double x1 = myPolygon.get(i).getX();
+           double y2 = myPolygon.get(i+1).getY();
+           total += x1 * y2;
+        }
+        for(int i = myPolygon.size() - 1; i < myPolygon.size(); i++){
+           double x1 = myPolygon.get(i).getX();
+           double y2 = myPolygon.get(0).getY();
+           total += x1 * y2;
+        }
+    area = Math.abs(0.5 * total);
+    return area;
+    }
 }
